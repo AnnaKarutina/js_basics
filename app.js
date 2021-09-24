5abd85d93dc7b23871471cc705f3a22fc4cf411d
Anna Karutina <anna.karutina@khk.ee>
2021-09-24 11:17:41 +0300

    Variables

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("test");
+// Muutujad
+/*
+vaatame
+kuidas 
+luua ja kasutada muutujad
+*/
+// muutuja, mille sisu saab muuta
+let name = 'Kadi Tamm';
+
+name = "Kadi Lepp";
+
+let fisrtName = 'Kadi';
+let $ = 'dollar';
+
+// muutuja, mis on konstantne
+const surName = 'Tamm';
+// surName = 'Lepp';
+
+// massiivid
+
+const numbers = [1, 2, 3, 4];
+numbers.push(6);
+
+// objektid
+const person = {
+	name: 'Kadi',
+	age: 20
+};
+
+person.age = 21;
+
+console.log(numbers);
+console.log(person);
