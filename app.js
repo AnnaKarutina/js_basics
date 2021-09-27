9715a130eec24a0459cb2bdb00ce2c6d820a8d95
Anna Karutina <anna.karutina@khk.ee>
2021-09-27 13:26:50 +0300

    Arrays and methods

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
-const firstname = 'Kadi';
-const surname = 'Tamm';
-const age = 20;
-const tags = 'arendus,veeb';
+const numbers = [43, 56, 33, 23, 44, 36, 5];
+const numbers2 = new Array(22, 45, 33, 76, 54);
+const fruits = ['õun', 'banaan', 'apelsiin', 'virsik'];
+const mixed = [22, 'Tere', true, undefined, null, {a:1, b:2}, new Date()];
+
 
 let val;
 
-// concatenation - es5
-val = '<ul>' + '<li>' + firstname + '</li>' + '<li>' + surname + '</li>' + '<li>' + age + '</li>' + '<li>' + tags + '</li>'+ '</ul>';
+val = numbers.length; // massiivi pikkus
+val = Array.isArray(numbers); // kas on massiiv
+val = numbers[3]; // kindel väärtus
 
-val = '<ul>' + 
-		'<li>' + firstname + '</li>' + 
-		'<li>' + surname + '</li>' + 
-		'<li>' + age + '</li>' + 
-		'<li>' + tags + '</li>'+ 
-	'</ul>';
+numbers[2] = 100; // asendamine
 
-// templating - es6
-val = `
-	<ul>
-		<li>Eesnimi: ${firstname}</li>
-		<li>Perenimi: ${surname}</li>
-		<li>Vanus: ${age}</li>
-		<li>Alad: ${tags}</li>
-	</ul>
-`;
+// lisamine
+numbers.push(250);
+numbers.unshift(120);
 
+// maha võtmine
+numbers.pop();
+numbers.shift();
 
+// index
+val = numbers.indexOf(100);
+
+// lõikamine
+numbers.splice(2, 1);
+
+// ühendamine
+val = numbers.concat(numbers2);
+
+// sorteerimine
+val = fruits.sort();
+// numbrid kasvujärjekorras
+val = numbers.sort(function(x, y){
+	return x - y;
+});
+// numbrid kahanemisjärjekorras
+val = numbers.sort(function(x, y){
+	return y - x;
+});
+
+console.log(numbers);
 console.log(val);
-document.body.innerHTML = val;
