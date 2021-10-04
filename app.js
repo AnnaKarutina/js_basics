4aa7357dacd6cf7c05e8d05579d11cefa1a7878f
Anna Karutina <anna.karutina@khk.ee>
2021-10-04 13:27:22 +0300

    JS events - click events

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 // event elements
-const form = document.querySelector('form');
-const taskInput = document.querySelector('#task');
+const list = document.querySelector('ul');
 
-// keyboad
-// taskInput.addEventListener('keydown', runEvent);
-// taskInput.addEventListener('keyup', runEvent);
-// taskInput.addEventListener('keypress', runEvent);
+console.log(list);
 
-// focus & blur
-// taskInput.addEventListener('focus', runEvent);
-// taskInput.addEventListener('blur', runEvent);
-
-//cut & paste
-taskInput.addEventListener('cut', runEvent);
-taskInput.addEventListener('paste', runEvent);
-
-// input by symbol
-taskInput.addEventListener('input', runEvent);
+// click by symbol
+list.addEventListener('click', runEvent);
 
 
 function runEvent(e) {
 	console.log(`Event is ${e.type}`);
 	// something to do
-	console.log(e.target.value);
+	
 }
